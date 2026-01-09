b7a6ed4cfeb2f21680c7bfdc0c64423338e67643
Syeda Alisha Zehra <161215836+alishazehra@users.noreply.github.com>
2026-01-10 00:04:49 +0500

    Delete src/app/root-client.tsx

--- src/app/root-client.tsx
+++
@@ -1,8 +0,0 @@
-// "use client";
-
-// import { AuthProvider } from "@/lib/auth";
-// import { ReactNode } from "react";
-
-// export function RootClient({ children }: { children: ReactNode }) {
-//   return <AuthProvider>{children}</AuthProvider>;
-// }
